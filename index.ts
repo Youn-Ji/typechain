a98096626593cb1fedd5ffd41ad17e1187676959
Youn_Ji <syj9760@gmail.com>
2021-02-27 15:14:49 +0900

    :zap: optional param '?'

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log('hello');
+const name =  'younji',
+  age = 28,
+  gender = 'female'
+
+const sayhi = (name, age, gender?) => {
+  console.log(`My name is ${name}. I'm ${age}. And I am ${gender}`)
+}
+
+sayhi(name, age)
+
+export {}

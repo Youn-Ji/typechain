e22c6bd00aa34b3c7ab07e14b8ef3ddd158dbc23
Youn_Ji <syj9760@gmail.com>
2021-02-28 23:25:41 +0900

    :art: not interface but class!

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,27 @@
-interface Human {
-  name: string,
-  age: number,
-  job: string
+// interface Human {
+//   name: string,
+//   age: number,
+//   job: string
+// }
+
+// const person = {
+//   name: "Song",
+//   age: 28,
+//   job: "student"
+// }
+
+class Human {
+  public name: string;
+  public age: number;
+  public job: string;
+  constructor(name: string, age: number, job: string) {
+    this.name = name;
+    this.age = age;
+    this.job = job;
+  }
 }
 
-const person = {
-  name: "Song",
-  age: 28,
-  job: "student"
-}
-
+const person = new Human('YounJi', 28, 'student');
 
 const sayhi = (person: Human): string => {
   return `My name is ${person.name}. I'm ${person.age}. And I am ${person.job}! lala`
